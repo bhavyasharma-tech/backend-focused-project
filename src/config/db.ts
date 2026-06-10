37db0c713bb9cb727e52d52795faaa6bf8655a21
Bhavya Sharma <bhavya3720@gmail.com>
2026-06-10 18:26:48 +0530

    feat: dockerize backend and integrate PostgreSQL and Redis

--- src/config/db.ts
+++ src/config/db.ts
@@ -15,11 +15,29 @@ export const sequelize = new Sequelize(
 );
 
 export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("PostgreSQL connected successfully");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    process.exit(1);
+  let retries = 10;
+
+  while (retries > 0) {
+    try {
+      await sequelize.authenticate();
+
+      console.log("PostgreSQL connected successfully");
+
+      return;
+    } catch (error) {
+      retries--;
+
+      console.log(
+        `Database not ready. Retrying in 5 seconds... (${retries} attempts left)`
+      );
+
+      await new Promise((resolve) =>
+        setTimeout(resolve, 5000)
+      );
+    }
   }
+
+  throw new Error(
+    "Could not connect to PostgreSQL after multiple attempts"
+  );
 };
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,20 +1,37 @@
 import { createClient } from "redis";
 
 export const redisClient = createClient({
-  url: "redis://localhost:6379",
+  url:  process.env.REDIS_URL||"redis://localhost:6379",
 });
 
 redisClient.on("error", (err) => {
   console.error("Redis Error:", err);
 });
 
-export const connectRedis = async () => {
-  try {
-    await redisClient.connect();
 
-    console.log("Redis connected successfully");
-  } catch (error) {
-    console.error("Redis connection failed:", error);
-    process.exit(1);
+export const connectRedis = async () => {
+  let retries = 10;
+
+  while (retries > 0) {
+    try {
+      await redisClient.connect();
+      console.log("Redis connected successfully");
+
+      return;
+    } catch (error) {
+      retries--;
+
+      console.log(
+        `Redis not ready. Retrying in 5 seconds... (${retries} attempts left)`
+      );
+
+      await new Promise((resolve) =>
+        setTimeout(resolve, 5000)
+      );
+    }
   }
+
+  throw new Error(
+    "Could not connect to Redis after multiple attempts"
+  );
 };
